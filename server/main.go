3b6e9f11e1cab9eb0a7111f7d067a79341c72700
b44ken <bradboratto@gmail.com>
2024-06-24 04:49:53 -0400

    pull out just planner

--- server/main.go
+++
@@ -1,6 +0,0 @@
-package main
-
-func main() {
-	database_connect()
-	start_server()
-}
--- uwinsite/util/soupoutput.py
+++
@@ -1,8 +0,0 @@
-import bs4, fetchdata, os
-
-id = os.environ.get('SESSION_ID')
-
-fetch = fetchdata.fetch_course_id('004801', '11', id)
-soup = bs4.BeautifulSoup(fetch, 'lxml')
-file = open('html/COMP1410-soup.html', 'w')
-file.write(str(soup))
